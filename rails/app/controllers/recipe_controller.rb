bd07830f8d4dd52206a92cc209c4105f8648c92c
naoki-shigehisa <naoki.shigehisa@gmail.com>
2021-05-15 21:27:37 +0900

    カクテルリストのデザイン変更

--- rails/app/controllers/recipe_controller.rb
+++ rails/app/controllers/recipe_controller.rb
@@ -1,12 +1,23 @@
 class RecipeController < ApplicationController
   def list
-    @recipes = Recipe.select(:id,:name)
+    recipes = Recipe
+                .select(:id,:name,:style_id,:tech_id,:alcohol_id)
+                .preload(:style,:tech,:alcohol)
+                .map{|r|
+                  {
+                    "id": r.id,
+                    "name": r.name,
+                    "style": r.style.name,
+                    "tech": r.tech.name,
+                    "alcohol": r.alcohol.name
+                  }
+                }
     @can_recipes = []
-    @recipes.each{|recipe|
+    recipes.each{|recipe|
       have_flag = RecipeMaterial
         .select(:id,:recipe_id,:material_id,:option_flag)
         .includes(:material)
-        .where("(recipe_id = ?) AND (option_flag = 0)", recipe.id)
+        .where("(recipe_id = ?) AND (option_flag = 0)", recipe["id"])
         .map{|r|
           r.material.have_flag
         }
