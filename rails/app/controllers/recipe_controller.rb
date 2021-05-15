8a1754d6159fb2ba39dad3dbccb03afe7b7de635
naoki-shigehisa <naoki.shigehisa@gmail.com>
2021-05-15 23:41:16 +0900

    カクテル詳細ページ作成

--- rails/app/controllers/recipe_controller.rb
+++ rails/app/controllers/recipe_controller.rb
@@ -17,7 +17,7 @@ class RecipeController < ApplicationController
       have_flag = RecipeMaterial
         .select(:id,:recipe_id,:material_id,:option_flag)
         .includes(:material)
-        .where("(recipe_id = ?) AND (option_flag = 0)", recipe["id"])
+        .where("(recipe_id = ?) AND (option_flag = 0)", recipe[:id])
         .map{|r|
           r.material.have_flag
         }
@@ -29,6 +29,23 @@ class RecipeController < ApplicationController
   end
 
   def detail
-    @recipe_detail = Recipe.find(params[:id])
+    @recipe_detail = Recipe
+                      .select(:id,:name,:style_id,:tech_id,:alcohol_id)
+                      .joins(:style,:tech,:alcohol)
+                      .find(params[:id])
+    @materials = RecipeMaterial
+                  .select(:recipe_id,:material_id,:amount,:option_flag)
+                  .preload(:material)
+                  .where("recipe_id = ?", params[:id])
+                  .map{|m|
+                    {
+                      "id": m.material.id,
+                      "name": m.material.name,
+                      "alcohol_flag": m.material.alcohol_flag,
+                      "have_flag": m.material.have_flag,
+                      "amount": m.amount,
+                      "option_flag": m.option_flag
+                    }
+                  }
   end
 end
