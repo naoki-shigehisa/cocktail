e9879d10738bdca47b472f46e5d699adb28869c4
naoki-shigehisa <naoki.shigehisa@gmail.com>
2021-05-16 11:06:17 +0900

    recipe listのリファクタリング

--- rails/app/controllers/recipe_controller.rb
+++ rails/app/controllers/recipe_controller.rb
@@ -1,31 +1,6 @@
 class RecipeController < ApplicationController
   def list
-    recipes = Recipe
-                .select(:id,:name,:style_id,:tech_id,:alcohol_id)
-                .preload(:style,:tech,:alcohol)
-                .map{|r|
-                  {
-                    "id": r.id,
-                    "name": r.name,
-                    "style": r.style.name,
-                    "tech": r.tech.name,
-                    "alcohol": r.alcohol.name
-                  }
-                }
-    @can_recipes = []
-    recipes.each{|recipe|
-      have_flag = RecipeMaterial
-        .select(:id,:recipe_id,:material_id,:option_flag)
-        .includes(:material)
-        .where("(recipe_id = ?) AND (option_flag = 0)", recipe[:id])
-        .map{|r|
-          r.material.have_flag
-        }
-        .all? {|v| v }
-      if have_flag
-        @can_recipes << recipe
-      end
-    }
+    @recipes = Recipe.can_recipes
   end
 
   def detail
--- rails/app/models/recipe.rb
+++ rails/app/models/recipe.rb
@@ -3,4 +3,34 @@ class Recipe < ApplicationRecord
     belongs_to :tech
     belongs_to :style
     belongs_to :alcohol
+
+    def self.can_recipes
+        recipes = self
+                    .select(:id,:name,:style_id,:tech_id,:alcohol_id)
+                    .preload(:style,:tech,:alcohol)
+                    .map{|r|
+                      {
+                        "id": r.id,
+                        "name": r.name,
+                        "style": r.style.name,
+                        "tech": r.tech.name,
+                        "alcohol": r.alcohol.name
+                      }
+                    }
+        @can_recipes = []
+        recipes.each{|recipe|
+          have_flag = RecipeMaterial
+            .select(:id,:recipe_id,:material_id,:option_flag)
+            .includes(:material)
+            .where("(recipe_id = ?) AND (option_flag = 0)", recipe[:id])
+            .map{|r|
+              r.material.have_flag
+            }
+            .all? {|v| v }
+          if have_flag
+            @can_recipes << recipe
+          end
+        }
+        @can_recipes
+    end
 end
