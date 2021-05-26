4c6ae76cc6b69f183ea8f5853ab8e7a5f944974c
naoki-shigehisa <naoki.shigehisa@gmail.com>
2021-05-26 19:49:04 +0900

    [WIP]create order/create controller

--- rails/app/controllers/orders/index_controller.rb
+++ rails/app/controllers/orders/index_controller.rb
@@ -2,4 +2,9 @@ class Orders::IndexController < ApplicationController
   def index
       @recipes = Orders.order_recipes_array
   end
+
+  def create
+    order = Order.new(recipe_id:params[:recipe_id], name_entered: params[:name_entered])
+    @recipe = Recipe.detail(order.recipe_id)
+  end
 end
