3daa33badc38a979b26724d2394272b7e933812c
naoki-shigehisa <naoki.shigehisa@gmail.com>
2021-05-30 19:22:39 +0900

    #33 トップ画面のデザイン修正

--- rails/app/controllers/application_controller.rb
+++ rails/app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
     def welcome
-        @current_user = User.current_user_id(cookies)
+        @current_user = User.current_user(cookies)
         render 'layouts/welcome'
     end
 
